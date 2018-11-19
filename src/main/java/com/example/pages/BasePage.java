60838a52a6397c1d62bde5748f12d784ba3b32a5
Sargis Sargsyan <sargis.sargsyan@live.com>
2018-11-19 16:55:09 +0400

    url changed

--- src/main/java/com/example/pages/BasePage.java
+++ src/main/java/com/example/pages/BasePage.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public abstract class BasePage<T extends LoadableComponent<T>> extends LoadableComponent<T>  {
 	private WebDriver driver;
 	private WebDriverWait wait;
-	private static final String BASE_URL = "http://10.39.18.93:8080";
+	private static final String BASE_URL = "https://sqa-days-example.herokuapp.com";
 
 	public BasePage() {
 		this.driver = DriverBase.get().getDriver();
